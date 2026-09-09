43a94ff32d5d870afdb5d750e9ab31c57844a7e4
Tigran Mkhitaryan <mkhitaryantigran435@gmail.com>
2026-09-09 14:00:53 +0400

    Add UI components

---
+++ client/src/components/ui/sonner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Toaster as Sonner } from "sonner";
+
+const Toaster = ({ ...props }) =>
+  React.createElement(Sonner, {
+    theme: "light",
+    className: "toaster group",
+    style: {
+      "--normal-bg": "var(--popover)",
+      "--normal-text": "var(--popover-foreground)",
+      "--normal-border": "var(--border)"
+    },
+    ...props
+  });
+
+export { Toaster };
